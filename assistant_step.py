d75b43ae1e185ea86c6503da47b1ee465b92cf92
arthur <arthur.wlodarczak@etu.univ-nantes.fr>
2024-12-24 17:27:54 +0100

    Mise en place technique du pilotage vocal de l’assistant  - Sound

--- assistant_step.py
+++ assistant_step.py
@@ -1,6 +1,17 @@
 import pyttsx3
 from urllib.request import urlopen
 import speech_recognition as sr
+import pygame
+
+
+pygame.mixer.init()
+
+
+
+def play_sound(string):
+    pygame.mixer.music.load(string) 
+    pygame.mixer.music.play()  
+
 
 def assistant_voice(output):
     if output!= None:
@@ -19,42 +30,54 @@ def internet():
         return False
 
 
-def recognition():
+def recognition(active):
     r = sr.Recognizer()
     r.energy_threshold = 4000
     
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         r.pause_threshold = 0.7
-
-        print("Vous pouvez parler maintenant...")  
+        if active:
+            play_sound("parler.wav")  
+            print("Vous pouvez parler maintenant...")  
             
         audio = r.listen(source)
         
+        if active: 
+            play_sound("envoi.wav")  
+
         if internet():
             try:
                 vocal = r.recognize_google(audio, language='fr-FR')
                 print(vocal)
                 return vocal
             except sr.UnknownValueError:
-                assistant_voice("Désolé, je n'ai pas compris.")
+                if active:  
+                    assistant_voice("Désolé, je n'ai pas compris.")
             except sr.RequestError as e:
-                assistant_voice(f"Erreur de service Google: {e}")
+                if active:
+                    assistant_voice(f"Erreur de service Google: {e}")
         else:
             try:
-                vocal = r.recognize_sphinx(audio, language='fr-FR')
+                vocal.recognize_sphinx(audio, language='fr-FR')
                 print(vocal)
                 return vocal
             except sr.UnknownValueError:
-                assistant_voice("Désolé, je n'ai pas compris.")
-                    
+                if active:
+                    assistant_voice("Désolé, je n'ai pas compris.")
                     
 def main():
-    assistant_voice("Bonjour, je suis votre assistant vocal.")
+    assistant_voice("Dîtes 'bonjour' pour activer mes services.")
+    trigger_word = "bonjour"  
+    active = False  
     while True:
-        input = recognition()  
-        if input :
-            assistant_voice(input)
+        input = recognition(active) 
+        if input:
+            if not active and trigger_word in input.lower():
+                assistant_voice("Activation réussie, comment puis-je vous aider ?")
+                active = True  
+            elif active :
+		            assistant_voice(input)
 
 
 if __name__ == '__main__':
